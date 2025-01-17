bae2878192a5ad884557e5dc3b7372fa5d6b1cb6
Mitendra Kumar Raikwar <161349381+mitendra9315@users.noreply.github.com>
2025-01-17 20:37:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return '<h1> Welcome </h1>'
+
+@app.route('/')
+def about():
+    return '<h1> About Us </h1>'
+
+if __name__=='__main__':
+    port = int(os.environ.get("PORT",5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
